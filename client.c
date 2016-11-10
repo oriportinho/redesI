dd33ae8851e8188ee18a589c95624d2a5e648ca6
oriportinho <ori.portinho@gmail.com>
2016-11-10 20:57:01 -0200

    Quase tudo pronto, loop infinito

--- client.c
+++ client.c
@@ -55,7 +55,81 @@ void client(int port) {
 
 }
 
+void server(int port) {
+  // char buffer[100];
+  int sockfd, newsockfd, listen_fd;
+  socklen_t clilen;
 
-int main(int argc,char **argv) {
+  struct sockaddr_in servaddr, cli_addr;
+  int pid;
+
+  listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  bzero(&servaddr, sizeof(servaddr));
+
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+  servaddr.sin_port = htons(port);
+
+  if(bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+    perror("ERRO on binding.\n");
+    exit(1);
+  }
+
+  listen(sockfd,5);
+  clilen = sizeof(cli_addr);
+
+	while(true) {
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+		if(newsockfd < 0) {
+			perror("ERROR on accept");
+			exit(1);
+		}
+
+		/* Create child process */
+		pid = fork();
+
+		if(pid < 0) {
+			perror("ERROR on fork");
+			exit(1);
+		}
+
+		if(pid == 0) {
+			close(sockfd);
+      int n;
+      char bufferb[256];
+
+      bzero(bufferb, 256);
+      n = read(newsockfd, bufferb, 255);
+
+      if(n < 0) {
+        perror("ERRO reading from socket.\n");
+        exit(1);
+      }
+
+			printf("Stranger: %s\n", bufferb);
+
+			exit(0);
+
+		}else {
+			close(newsockfd);
+		}
+
+	}
+}
+
+int main(int argc, char *argv[]) {
+  int pid = fork();
+  int port_server = atoi(argv[1]);
+
+  if(pid == 0) {
+    server(port_server);
+  }else {
+    int port_client;
+    printf("What is a destiny port?\n");
+    scanf("%d\n", &port_client);
+    client(port_client);
+  }
 
 }
